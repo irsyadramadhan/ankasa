12318a19ca161e7efbdffe27fdbe7a668e1f1e1f
Irsyad <ramadhanirsyad@icloud.com>
2023-05-03 08:35:09 +0700

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,19 +10,19 @@ export default function App() {
         <div>
           <div className="flex">
             <div className="w-6/12">
-              <div>
+              <div className="ml-24">
                 <span className="text-black text-4xl font-semibold">Find your <span className="text-blue-500">Flight</span></span>
               </div>
-              <div>
+              <div className="ml-24">
                 <span className="text-gray-500 text-xl">and explore the world with us</span>
               </div>
               <div>
-                <Image src="/img-landing.png" alt="landing" width={1100} height={500} />
+                <Image src="/tokyo-1.png" alt="tokyo" width={1000} height={500} />
               </div>
             </div>
             <div className="w-6/12">
               <div>
-                <Image src="/img-landing.png" alt="landing" width={1100} height={500} />
+                <Image src="/tokyo.png" alt="landing" width={1000} height={500} />
               </div>
             </div>
           </div>
@@ -36,40 +36,52 @@ export default function App() {
               <span className="text-gray-500">Explore the world with us.</span>
             </div>
             <div>
-              <span className="text-gray-500">Ankasa. All rights reserved.</span>
+              <span className="text-gray-500">&copy;Ankasa. All rights reserved.</span>
             </div>
           </div>
           <div className="w-3/12">
             <div>
-              <Logo />
+              <span className="text-black font-semibold">Features</span>
             </div>
             <div>
-              <span className="text-gray-500">Explore the world with us.</span>
+              <a className="text-gray-500">Find Ticket</a>
             </div>
             <div>
-              <span className="text-gray-500">Ankasa. All rights reserved.</span>
-            </div>
-          </div>
-          <div className="w-3/12">
-            <div>
-              <Logo />
+              <a className="text-gray-500">My Booking</a>
             </div>
             <div>
-              <span className="text-gray-500">Explore the world with us.</span>
+              <a className="text-gray-500">Chat</a>
             </div>
             <div>
-              <span className="text-gray-500">Ankasa. All rights reserved.</span>
+              <a className="text-gray-500">Notification</a>
             </div>
           </div>
           <div className="w-3/12">
             <div>
-              <Logo />
+              <span className="text-black font-semibold">Download App</span>
             </div>
             <div>
-              <span className="text-gray-500">Explore the world with us.</span>
+              <Image src="/appstore.png" alt="logo" width={158} height={36} />
             </div>
             <div>
-              <span className="text-gray-500">Ankasa. All rights reserved.</span>
+              <Image src="/googleplay.png" alt="logo" width={158} height={36} />
+            </div>
+          </div>
+          <div className="w-3/12">
+            <div>
+              <span className="text-black font-semibold">Follow Us</span>
+            </div>
+            <div>
+              <Image src="/facebook.png" alt="logo" width={24} height={24} />
+            </div>
+            <div>
+              <Image src="/twitter.png" alt="logo" width={24} height={24} />
+            </div>
+            <div>
+              <Image src="/instagram.png" alt="logo" width={24} height={24} />
+            </div>
+            <div>
+              <Image src="/youtube.png" alt="logo" width={24} height={24} />
             </div>
           </div>
         </div>
